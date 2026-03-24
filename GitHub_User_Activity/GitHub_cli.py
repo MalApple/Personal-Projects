e27deed957c8c3b25a4ea3c861dfae6b9e0c9934
Malachi Berry <malapplekey@gmail.com>
2026-03-24 12:19:34 +1030

    Added the ability to get events from github for a specific user

--- GitHub_User_Activity/GitHub_cli.py
+++ GitHub_User_Activity/GitHub_cli.py
@@ -1,1 +1,35 @@
 #A command line interface to fetch user activity from github, displaying recent activity with time and date
+import sys
+import json
+from urllib import request
+
+def userInput() -> str:
+    user_name = sys.argv[1]
+    return user_name
+
+def buildURL(user_name) -> str:
+    url = f"https://api.github.com/users/{user_name}/events"
+    return url
+
+def fetchData(url) -> dict:
+    response = request.urlopen(url)
+    data = response.read()
+    events = json.loads(data)
+    return events
+
+
+def outputData(events):
+    print(events[:5])
+
+
+
+
+
+
+url = buildURL(userInput())
+events = fetchData(url)
+outputData(events)
+
+
+
+    
